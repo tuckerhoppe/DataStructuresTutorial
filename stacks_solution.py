721ab52151c4c0ff2797eae93b5cc3359da8bd67
Tucker Hoppe <74329199+tuckerhoppe@users.noreply.github.com>
2022-07-12 23:12:43 -0600

    Create stacks_solution.py

---
+++ stacks_solution.py
@@ -0,0 +1,15 @@
+# Stacks problem solution
+
+letters = []
+letter = "o"
+
+print("enter (undo) to remove the last thing entered from the stack")
+
+while letter != "x"
+  letter = input()
+  if letter = "undo":
+    print("Last thing entered was: ", letters.pop())
+  else:
+    letters.append()
+    
+print(letters)
